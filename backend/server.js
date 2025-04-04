a0b272bea454bca4ae522509ba082ff584ae24aa
Pakwan SK <pakwan@example.com>
2025-04-04 15:53:52 +0700

    Delete Products

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,18 @@ app.post("/api/products", async(req,res) => {
         res.status(500).json({success:false, message: "Server Error"});
     }
 
-})
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+    const {id} = req.params;
+    
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({success: true, message: "Product deleted"});
+    } catch (error) {
+        res.status(404).json({success: false, message: "Product not found"})
+    }
+});
 
 app.listen(5000, () => {
     connectDB();
