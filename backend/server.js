b461a6d6db78d7e92add0f62353be45ffbc1b57d
Pakwan SK <pakwan@example.com>
2025-04-05 14:09:28 +0700

    Update Backend

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import productRoute from "./routes/product.route.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 
 app.use(express.json()); // accept JSON data in the rq.body
@@ -13,8 +14,8 @@ app.use(express.json()); // accept JSON data in the rq.body
 app.use("/api/products", productRoute); // use the product route
 
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("Sever started at http://localhost:5000");
+    console.log("Sever started at http://localhost:"+ PORT);
 });
 
